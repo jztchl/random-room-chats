05869654492f191e4160fe40529db589201a077e
jztchl <kmvishnu625@gmail.com>
2025-07-09 21:41:01 +0530

    added procfile

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: daphne django_chat_room.asgi:application
--- random_chat/settings.py
+++ random_chat/settings.py
@@ -129,7 +129,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-print(os.getenv("REDIS_URL"))
 if DEBUG:
     CHANNEL_LAYERS = {
         "default": {
