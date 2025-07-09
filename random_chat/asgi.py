99ce0933ccbc4b2794b60e45d83fecc73d0ac4ae
jztchl <kmvishnu625@gmail.com>
2025-07-09 17:56:32 +0530

    random chats

---
+++ random_chat/asgi.py
@@ -0,0 +1,26 @@
+
+import os
+from django.core.asgi import get_asgi_application
+
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+from django.core.asgi import get_asgi_application
+
+from random_chat.routing import websocket_urlpatterns
+
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'random_chat.settings')
+
+django_asgi_app = get_asgi_application()
+
+
+
+application = ProtocolTypeRouter(
+    {
+        "http": django_asgi_app,
+        "websocket": AllowedHostsOriginValidator(
+            AuthMiddlewareStack(URLRouter(websocket_urlpatterns))
+        ),
+    }
+)
