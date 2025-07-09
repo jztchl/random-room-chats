5bf3c04a670acdbc5058f007d50c67b432963fb8
jztchl <kmvishnu625@gmail.com>
2025-07-09 22:48:00 +0530

    asgi

--- random_chat/asgi.py
+++ random_chat/asgi.py
@@ -1,11 +1,12 @@
 import os
 import django
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'random_chat.settings')
+django.setup()
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
 from chat.routing import websocket_urlpatterns
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'random_chat.settings')
-django.setup()
+
 django_asgi_app = get_asgi_application()
 
 
