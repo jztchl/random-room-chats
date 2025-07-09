0394dbf6b4f5ea9c839e7afaa44154a2ea290a56
jztchl <kmvishnu625@gmail.com>
2025-07-09 22:44:51 +0530

    some dependency problems solved'

--- chat/models.py
+++ chat/models.py
@@ -4,7 +4,4 @@ class Room(models.Model):
     name = models.CharField(max_length=100,unique=True)
     members = models.PositiveIntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
-    
-    class Meta:
-        app_label = 'chat'
         
--- random_chat/asgi.py
+++ random_chat/asgi.py
@@ -1,17 +1,18 @@
 import os
-from django.core.asgi import get_asgi_application
+import django
 from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.security.websocket import AllowedHostsOriginValidator
-
+from django.core.asgi import get_asgi_application
+from channels.auth import AuthMiddlewareStack
 from chat.routing import websocket_urlpatterns
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'random_chat.settings')
-
+django.setup()
 django_asgi_app = get_asgi_application()
+
+
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
-    "websocket": AllowedHostsOriginValidator(
-      URLRouter(websocket_urlpatterns)
+    "websocket": AuthMiddlewareStack(
+        URLRouter(websocket_urlpatterns)
     ),
 })
