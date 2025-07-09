f59d25ec18abe0b99b2ca7aeacc1ea5340b01b95
jztchl <kmvishnu625@gmail.com>
2025-07-09 22:26:08 +0530

    one more fix related to asgi

--- random_chat/asgi.py
+++ random_chat/asgi.py
@@ -2,7 +2,6 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
-from channels.auth import AuthMiddlewareStack 
 
 from chat.routing import websocket_urlpatterns
 
@@ -13,8 +12,6 @@ django_asgi_app = get_asgi_application()
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
     "websocket": AllowedHostsOriginValidator(
-        AuthMiddlewareStack(
-            URLRouter(websocket_urlpatterns)
-        )
+      URLRouter(websocket_urlpatterns)
     ),
 })
